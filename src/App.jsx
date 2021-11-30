e275163561a4c79d80d41cd361d233772f812ac2
Juan Valero <valerojuan13c@gmail.com>
2021-11-30 10:12:42 -0300

    info, services, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages";
+import SigninPage from "./pages/singin";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Router>
-      <Home />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <Routes>
+        <Route path="/" element={<Home />} exac />
+        <Route path="/singin" element={<SigninPage />} exac />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
 import {
   Nav,
   NavbarContainer,
@@ -12,31 +13,50 @@ import {
   NavBtnLink,
 } from "./NavbarElements";
 
-const Navbar = ({toggle}) => {
+const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();   
+  };
+  
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to="/">dolla</NavLogo>
+          <NavLogo to="/" onClick={toggleHome}>dolla</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="/about">About</NavLinks>
+              <NavLinks to="about" smooth={true} duration={500}>About</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/discover">Discover</NavLinks>
+              <NavLinks to="discover">Discover</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/services">Services</NavLinks>
+              <NavLinks to="services">Services</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="/singup">Sing Up</NavLinks>
+              <NavLinks to="singup">Sing Up</NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
-            <NavBtnLink to="/singup">Sing Up</NavBtnLink>
+            <NavBtnLink to="singup">Sing Up</NavBtnLink>
           </NavBtn>
         </NavbarContainer>
       </Nav>
